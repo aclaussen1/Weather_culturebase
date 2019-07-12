590ed6f7c8cc5cd93afa463d1e90cf0e9f313303
alex claussen <aclaussen@ymail.com>
2019-07-12 12:06:57 -0400

    Initial Commit

---
+++ Weather_Culturebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_Culturebase
+//
+//  Created by Alexander Claussen on 7/12/19.
+//  Copyright © 2019 Alexander Claussen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
